26fcd84a63984465a5a96e7ef2d6008cce4e4e54
0xMRTT <0xMRTT@tuta.io>
2022-08-13 20:39:26 +0200

    chore: add default and use env vars in main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 
+DESTINATION_PATH = os.environ["INPUT_DESTINATION_PATH"]
+INPUT_PO_DIR = os.environ["INPUT_PO_DIR"]
+
 def list_po():
     files = []
-    for _, _, filesname in os.walk("po"):
+    for _, _, filesname in os.walk(INPUT_PO_DIR):
         for file  in filesname:
             if file.endswith(".po"):
                 files.append(file.strip(".po"))
     return files
     
 
-with open("LINGUAS", "w") as file:
+with open(f"{DESTINATION_PATH}/LINGUAS", "w") as file:
     po = "\n".join(list_po())
     file.write(po)
 
